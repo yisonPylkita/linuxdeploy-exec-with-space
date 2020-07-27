c5511631d4d6137a5c1d25cfb28e26e1a3172313
Wojciech Bartnik <wbartnik@smartscore.pl>
2020-07-27 16:39:55 +0200

    Actually I don't even need to create an AppImage to reproduce this issue

--- build-me.sh
+++ build-me.sh
@@ -16,4 +16,4 @@ mv Yin_yang.svg "App with a space in name.svg"
 # Download lastest linuxdeploy and run it against this project
 wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
 chmod +x linuxdeploy*.AppImage
-./linuxdeploy-x86_64.AppImage --create-desktop-file --appdir=AppDir -e "App with a space in name" -i "App with a space in name.svg" --output appimage
+./linuxdeploy-x86_64.AppImage --create-desktop-file --appdir=AppDir -e "App with a space in name" -i "App with a space in name.svg"
